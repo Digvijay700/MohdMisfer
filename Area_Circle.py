21e50e1fc4192e667fe12897d6134e9f9882826d
Misfer4710 <88150232+Digvijay700@users.noreply.github.com>
2024-09-07 16:45:41 +0530

    Add files via upload

---
+++ Area_Circle.py
@@ -0,0 +1,5 @@
+from math import pi
+
+radius=float(input("Enter the radius of the circle:"))
+area=pi*radius**2
+print("The area of the cricle is" + str(area))
---
+++ ifCondition.py
@@ -0,0 +1,6 @@
+anw=input("Is it raining?")
+
+if anw=="yes":
+    print("Take an umbrella")
+else:
+    print("Put on a hat")
